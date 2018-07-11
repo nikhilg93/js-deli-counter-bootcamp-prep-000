a31f4cbf786c0d1b4bacefb7d9f299f13205475d
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-07-11 19:08:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,13 +23,21 @@ var line = [];
 
 function currentLine(katzDeliLine){
   for (let j=0; j <katzDeliLine.length; j++){
+<<<<<<< HEAD
     line.push(j+1+'.'+' '+ katzDeliLine[j]);
+=======
+    line.push(j+1+'.'+katzDeliLine[j]);
+>>>>>>> 13a5d4dc099af21d821d6e07b39fb08659995fbe
   }
   if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }
   else{
+<<<<<<< HEAD
     return ("The line is currently: " + line.join(', '))
+=======
+    return ("The line is currently:" + line)
+>>>>>>> 13a5d4dc099af21d821d6e07b39fb08659995fbe
   }
   
 }
@@ -37,7 +45,11 @@ function currentLine(katzDeliLine){
   
 
 
+<<<<<<< HEAD
 /*takeANumber(katzDeliLine, "Ada");
+=======
+takeANumber(katzDeliLine, "Ada");
+>>>>>>> 13a5d4dc099af21d821d6e07b39fb08659995fbe
 takeANumber(katzDeliLine, "Grace");
 takeANumber(katzDeliLine, "Kent");
 
@@ -50,4 +62,8 @@ currentLine(katzDeliLine);
 takeANumber(katzDeliLine, "Matz");
 currentLine(katzDeliLine);
 nowServing(katzDeliLine);
+<<<<<<< HEAD
 currentLine(katzDeliLine);*/
+=======
+currentLine(katzDeliLine);
+>>>>>>> 13a5d4dc099af21d821d6e07b39fb08659995fbe
